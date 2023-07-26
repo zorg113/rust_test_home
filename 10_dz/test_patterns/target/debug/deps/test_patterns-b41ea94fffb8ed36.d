0eb9af48a61c87187eee45d5c2a589c0b773330a
dmitryinaz <zorgn@mail.ru>
2023-07-26 10:15:07 +0300

    rust_patterns

---
+++ 10_dz/test_patterns/target/debug/deps/test_patterns-b41ea94fffb8ed36.d
@@ -0,0 +1,7 @@
+e:\otus\home\rust_test_home\10_dz\test_patterns\target\debug\deps\test_patterns-b41ea94fffb8ed36.rmeta: src\lib.rs src\dynamic_v.rs src\static_v.rs
+
+e:\otus\home\rust_test_home\10_dz\test_patterns\target\debug\deps\test_patterns-b41ea94fffb8ed36.d: src\lib.rs src\dynamic_v.rs src\static_v.rs
+
+src\lib.rs:
+src\dynamic_v.rs:
+src\static_v.rs:
---
+++ 12_dz/test_rust_patterns/target/debug/examples/use_static-a1cc460a7c4f8c98.d
@@ -0,0 +1,5 @@
+e:\otus\home\rust_test_home\10_dz\test_patterns\target\debug\examples\use_static-a1cc460a7c4f8c98.rmeta: examples\use_static.rs
+
+e:\otus\home\rust_test_home\10_dz\test_patterns\target\debug\examples\use_static-a1cc460a7c4f8c98.d: examples\use_static.rs
+
+examples\use_static.rs:
